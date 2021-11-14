296ba511add9ea459ba956c5b095ff6a21a3b8d9
Andrey Martyn <martynandrey@rambler.ru>
2021-11-14 14:25:19 +0600

    Implemented a method for counting the number of squares from a given range

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,5 +1,24 @@
 package ru.netology.sqr;
 
 public class SQRService {
-
+    public int sqrInLimitsCount(int leftLimit, int rightLimit) {
+        // If the borders are set incorrectly, we swap them
+        if (leftLimit > rightLimit) {
+            int buffer = leftLimit;
+            leftLimit = rightLimit;
+            rightLimit = buffer;
+        }
+        int count = 0;
+        for (int i = 10; i < 100; i++) {
+            int sqr = i * i;
+            if (leftLimit <= sqr && sqr <= rightLimit) {
+                count++;
+            }
+            // If the square of the number is larger than the right border, there is no point in continuing
+            if (sqr > rightLimit) {
+                return count;
+            }
+        }
+        return count;
+    }
 }
